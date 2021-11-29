b29dfadcdd789403fcc69aee9e7f512871397453
Mohsen Mehrani <m.mehrani14@gmail.com>
2021-11-29 12:28:10 +0330

    R now is determined by the p_jm value

--- AverageFidelity.py
+++ AverageFidelity.py
@@ -1,9 +1,10 @@
-import numpy
+import numpy as np
 
 # calculate the average fidelity for a given m
-def averageOfFidelity(Data_Array, k_m, n_m, R):
-    Pacc_Array = [sum(i) for i in Data_Array] / n_m
-    PaccR_Array = [x ** R for x in Pacc_Array]
-    Lower_Bound_F = [1 / (R * x) for x in PaccR_Array]
+def averageOfFidelity(Data_Array, k_m, n_m):
+    Pacc_Array = np.array( [sum(i) for i in Data_Array] ) / n_m
+    R_Array = np.array( [int(1/np.log(1/p_jm)+0.5) for p_jm in Pacc_Array] ) #nearest integer is required
+    PaccR_Array = [Pacc_Array[i] ** R_Array[i] for i in range(len(Pacc_Array))]
+    Lower_Bound_F = [1 - (R_Array[i] * Pacc_Array[i])**(-1) for i in range(len(PaccR_Array))]
     AvergeOf_F = sum(Lower_Bound_F) / k_m
     return AvergeOf_F
