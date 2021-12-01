5e6652d65e28910128d5bf860687ac44cf0ee112
kasramasoudi <masoudi.kasra@gmail.com>
2021-12-01 14:22:14 +0330

    fixed bug that about the situation where all probabilities are equal to one

--- AverageFidelity.py
+++ AverageFidelity.py
@@ -3,7 +3,9 @@ import numpy as np
 # calculate the average fidelity for a given m
 def averageOfFidelity(Data_Array, k_m, n_m):
     Pacc_Array = np.array( [sum(i) for i in Data_Array] ) / n_m
-    R_Array = np.array( [int(1/np.log(1/p_jm)+0.5) for p_jm in Pacc_Array] ) #nearest integer is required
+    if k_m == sum(Pacc_Array):
+        return 1
+    R_Array = np.array([int(1/np.log(1/p_jm)+0.5) for p_jm in Pacc_Array] ) #nearest integer is required
     PaccR_Array = [Pacc_Array[i] ** R_Array[i] for i in range(len(Pacc_Array))]
     Lower_Bound_F = [1 - (R_Array[i] * Pacc_Array[i])**(-1) for i in range(len(PaccR_Array))]
     AvergeOf_F = sum(Lower_Bound_F) / k_m
