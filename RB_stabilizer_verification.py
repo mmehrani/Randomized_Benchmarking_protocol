1561175b31b90242d9a53bbf6f0f88e27ae8dd54
Mohsen Mehrani <m.mehrani14@gmail.com>
2021-12-08 15:56:18 +0330

    Hadamard is naughty

--- RB_stabilizer_verification.py
+++ RB_stabilizer_verification.py
@@ -7,7 +7,7 @@
 
 
 from pyquil import get_qc, Program
-from pyquil.gates import CNOT, Z, H,S, MEASURE, I
+from pyquil.gates import *
 from pyquil.api import local_forest_runtime
 from pyquil.quilbase import Declare
 from pyquil.simulation.tools import lifted_gate, program_unitary
@@ -26,9 +26,9 @@ from AverageFidelity import averageOfFidelity
 
 
 if __name__ == "__main__":
-    num_qubits = 2
+    num_qubits = 3
     #First step choose m and the K_m sequences of Clifford group
-    m = 10
+    m = 5
     k_m = 10 #n. of diff sequences
     n_m = 10  #n. of samples from a certain sequence
 
@@ -41,12 +41,98 @@ def generate_clifford_group(num_qubits):
     clifford_glossary = []
     clifford_glossary.extend([CNOT(i,j) for i in range(num_qubits) for j in range(num_qubits)])
     for i in range(num_qubits): clifford_glossary.remove(CNOT(i,i))
-#     clifford_glossary.extend([H(i) for i in range(num_qubits)])
+    clifford_glossary.extend([H(i) for i in range(num_qubits)])
     clifford_glossary.extend([S(i) for i in range(num_qubits)])
     return clifford_glossary
 
 
 # In[5]:
+
+
+def stab_transform(current_stab, gate_in_circuit):
+    if gate_in_circuit.name == 'H':
+        performing_qubit = gate_in_circuit.qubits[0].index #since it is a single-qubit
+        if current_stab[performing_qubit].name == 'Z':
+            current_stab[performing_qubit] = X(performing_qubit)
+        elif current_stab[performing_qubit].name == 'X':
+            current_stab[performing_qubit] = Z(performing_qubit)
+        elif current_stab[performing_qubit].name == 'Y':
+            current_stab[performing_qubit] = Y(performing_qubit)
+        elif current_stab[performing_qubit].name == 'I':
+            current_stab[performing_qubit] = I(performing_qubit)
+        else:
+            print(gate_in_circuit.name, current_stab)
+            raise Exception('This is the exception you expect to handle')
+    
+    elif gate_in_circuit.name == 'S':
+        performing_qubit = gate_in_circuit.qubits[0].index #since it is a single-qubit
+        if current_stab[performing_qubit].name == 'Z':
+            current_stab[performing_qubit] = Z(performing_qubit)
+        elif current_stab[performing_qubit].name == 'X':
+            current_stab[performing_qubit] = Y(performing_qubit)
+        elif current_stab[performing_qubit].name == 'Y':
+            current_stab[performing_qubit] = X(performing_qubit)
+        elif current_stab[performing_qubit].name == 'I':
+            current_stab[performing_qubit] = I(performing_qubit)
+        else:
+            print(gate_in_circuit.name, current_stab)
+            raise Exception('This is the exception you expect to handle')
+
+            
+    elif gate_in_circuit.name == 'CNOT':
+        performing_qubits = [qubit.index for qubit in gate_in_circuit.qubits] #since it is a single-qubit
+        stabs_names = [qubit.name for qubit in current_stab[performing_qubits]]
+        if stabs_names == ['Z','I']:
+            current_stab[performing_qubits] = [Z(performing_qubits[0]), I(performing_qubits[1])]
+        elif stabs_names == ['X','I']:
+            current_stab[performing_qubits] = [X(performing_qubits[0]), X(performing_qubits[1])]
+        elif stabs_names == ['Y','I']:
+            current_stab[performing_qubits] = [Y(performing_qubits[0]), X(performing_qubits[1])]
+        elif stabs_names == ['I','I']:
+            current_stab[performing_qubits] = [I(performing_qubits[0]), I(performing_qubits[1])]
+
+        elif stabs_names == ['Z','Z']:
+            current_stab[performing_qubits] = [I(performing_qubits[0]), Z(performing_qubits[1])]
+        elif stabs_names == ['X','Z']:
+            current_stab[performing_qubits] = [Y(performing_qubits[0]), Y(performing_qubits[1])]
+        elif stabs_names == ['Y','Z']:
+            current_stab[performing_qubits] = [X(performing_qubits[0]), Y(performing_qubits[1])]
+        elif stabs_names == ['I','Z']:
+            current_stab[performing_qubits] = [Z(performing_qubits[0]), Z(performing_qubits[1])]
+
+        elif stabs_names == ['Z','X']:
+            current_stab[performing_qubits] = [Z(performing_qubits[0]), X(performing_qubits[1])]
+        elif stabs_names == ['X','X']:
+            current_stab[performing_qubits] = [X(performing_qubits[0]), I(performing_qubits[1])]
+        elif stabs_names == ['Y','X']:
+            current_stab[performing_qubits] = [Y(performing_qubits[0]), I(performing_qubits[1])]
+        elif stabs_names == ['I','X']:
+            current_stab[performing_qubits] = [I(performing_qubits[0]), X(performing_qubits[1])]
+
+        elif stabs_names == ['Z','Y']:
+            current_stab[performing_qubits] = [Z(performing_qubits[0]), Y(performing_qubits[1])]
+        elif stabs_names == ['X','Y']:
+            current_stab[performing_qubits] = [Y(performing_qubits[0]), Z(performing_qubits[1])]
+        elif stabs_names == ['Y','Y']:
+            current_stab[performing_qubits] = [X(performing_qubits[0]), Z(performing_qubits[1])]
+        elif stabs_names == ['I','Y']:
+            current_stab[performing_qubits] = [Z(performing_qubits[0]), Y(performing_qubits[1])]
+        else:
+            print(gate_in_circuit.name, current_stab)
+            raise Exception('This is the exception you expect to handle')
+    else:
+        print(gate_in_circuit.name, current_stab)
+        raise Exception('This is the exception you expect to handle')
+    return current_stab
+
+def update_stabilizer(init_stab, gates_sequence):
+    stab = init_stab
+    for layer in gates_sequence:
+        stab = stab_transform( stab, layer )
+    return stab
+
+
+# In[6]:
 
 
 def machine_response_stabilizer_bench(num_qubits, m, k_m, n_m):
@@ -66,17 +152,13 @@ def machine_response_stabilizer_bench(num_qubits, m, k_m, n_m):
 
         c_jm_unitary = program_unitary(prog, n_qubits= num_qubits)
 
-        #report one random adapted stabilizer
-        s_i = np.identity(2**num_qubits)
+        initial_stabilizer = []
         for q_num in range(num_qubits):
-            s_i = np.matmul( lifted_gate( np.random.choice([I(q_num), Z(q_num)]), n_qubits = num_qubits ), s_i )
-        s_i = np.matmul(np.matmul(c_jm_unitary, s_i), np.conj(c_jm_unitary) )
-        stab_i_definition = DefGate("STAB_i", s_i)
-        STAB_i = stab_i_definition.get_constructor() # Get the gate constructor
-
-        n_tuple = tuple(range(num_qubits))
-        prog += Program( stab_i_definition, STAB_i(*n_tuple) )
-
+            initial_stabilizer.append( np.random.choice([Z(q_num), Z(q_num)]) )
+        stabilizer_layer = update_stabilizer( np.array(initial_stabilizer), c_jm )
+        
+        prog+= Program(*stabilizer_layer)
+        
         #Measurments
         ro = prog.declare('ro', 'BIT', num_qubits)
         for q in range(num_qubits):
@@ -108,10 +190,10 @@ def machine_response_stabilizer_bench(num_qubits, m, k_m, n_m):
 
 
 
-# In[6]:
+# In[7]:
 
 
-get_ipython().system('ipython nbconvert --to python --inplace RB_stabilizer_verification.ipynb')
+get_ipython().system('ipython nbconvert --to python RB_stabilizer_verification.ipynb')
 
 
 # In[ ]:
