e9f013eb9fc1aebac4250aba050e4202c03b2b3a
Mohsen Mehrani <m.mehrani14@gmail.com>
2022-12-26 09:43:48 +0330

    new lines added

--- functions.py
+++ functions.py
@@ -80,4 +80,84 @@ def daggered_gate(gate):
     
     else :
         raise ValueError("This gate daggered is not yet considered!")
+
+
+def convert_to_bloch_vector(rho):
+    """Convert a density matrix to a Bloch vector."""
+    X = np.array([[0, 1], [1, 0]])
+    Y = np.array([[0, -1j], [1j, 0]])
+    Z = np.array([[1, 0], [0, -1]])
+
+    # Used the mixed state simulator so we could have the density matrix for this part!
+    ax = np.trace(np.dot(rho, X)).real
+    ay = np.trace(np.dot(rho, Y)).real
+    az = np.trace(np.dot(rho, Z)).real
+    return [ax, ay, az]
+
+def plot_bloch_sphere(bloch_vectors):
+    """ Helper function to plot vectors on a sphere."""
+    fig = plt.figure(figsize=(6, 6))
+    ax = fig.add_subplot(111, projection='3d')
+    fig.subplots_adjust(left=0, right=1, bottom=0, top=1)
+
+    ax.grid(False)
+    ax.set_axis_off()
+    ax.view_init(30, 45)
+    ax.dist = 7
+
+    # Draw the axes (source: https://github.com/matplotlib/matplotlib/issues/13575)
+    x, y, z = np.array([[-1.5,0,0], [0,-1.5,0], [0,0,-1.5]])
+    u, v, w = np.array([[3,0,0], [0,3,0], [0,0,3]])
+    ax.quiver(x, y, z, u, v, w, arrow_length_ratio=0.05, color="black", linewidth=0.5)
+
+    ax.text(0, 0, 1.7, r"|0⟩", color="black", fontsize=16)
+    ax.text(0, 0, -1.9, r"|1⟩", color="black", fontsize=16)
+    ax.text(1.9, 0, 0, r"|+⟩", color="black", fontsize=16)
+    ax.text(-1.7, 0, 0, r"|–⟩", color="black", fontsize=16)
+    ax.text(0, 1.7, 0, r"|i+⟩", color="black", fontsize=16)
+    ax.text(0,-1.9, 0, r"|i–⟩", color="black", fontsize=16)
+
+    ax.scatter(
+        bloch_vectors[:,0], bloch_vectors[:,1], bloch_vectors[:, 2], c='#e29d9e', alpha=0.3
+    )
+
+G = Program( CPHASE01(-np.pi/2, control=0, target=1), CPHASE10(-np.pi/2, control=0, target=1) )
+
+def arbitary_single_qubit_circuit(theta, phi, si, qubit):
+    return Program( RZ(si, qubit = qubit), RY(phi, qubit = qubit), RZ(theta, qubit = qubit) )
+
+def r_theta_phi_rotation(theta, phi, qubit):
+    return arbitary_single_qubit_circuit( - phi/2, theta, phi/2, qubit)
+
+def give_random_single_qubit_gate(qubit):
+    theta, si = np.random.uniform(0,2*np.pi, size = 2)
     
+    phi_range = np.linspace(0,np.pi)
+    p_phi = np.sin(phi_range) / np.sum(np.sin(phi_range))
+    phi = np.random.choice(phi_range, p = p_phi)
+    return arbitary_single_qubit_circuit(theta, phi, si, qubit = qubit)
+
+def normalized_abs_angle_dist(angle_range):
+    dist = np.pi - np.abs( np.pi - angle_range )
+    dist /= np.sum(dist)
+    return dist
+
+def give_v_circuit(alpha, beta, delta, qubits = [0,1]):
+    prog = Program(G,  r_theta_phi_rotation(alpha, 0, qubit =qubits[0]),
+                   r_theta_phi_rotation(3*np.pi/2,0, qubit =qubits[1]), G)
+    prog += Program( r_theta_phi_rotation(beta, np.pi/2, qubit = qubits[0]), 
+                    r_theta_phi_rotation(3*np.pi/2, delta, qubit = qubits[1]), G)
+    return prog
+
+def give_random_two_quibt_circuit(qubits):
+    a,b,c,d = [give_random_single_qubit_gate(qubit=qubit) for _ in range(2) for qubit in qubits]
+    
+    angles_range = np.linspace(0,2*np.pi)
+    alpha, beta, delta = np.random.choice(angles_range, p = normalized_abs_angle_dist(angles_range),
+                                          size = 3)
+    
+    prog = Program(a, b )
+    prog += give_v_circuit(alpha, beta, delta, qubits = [0,1])
+    prog += Program(c, d )
+    return prog
+
