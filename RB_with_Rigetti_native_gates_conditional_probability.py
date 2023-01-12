a5ac215aa892c1f2034e7504a6ff82ae528e6574
Mohsen Mehrani <m.mehrani14@gmail.com>
2023-01-12 14:05:24 +0330

    some names changed

--- RB_with_Rigetti_native_gates_conditional_probability.py
+++ RB_with_Rigetti_native_gates_conditional_probability.py
@@ -2,18 +2,19 @@
 # coding: utf-8
 
 # # Benchmark with conditional formating
-# all we need in the protocal is the random unitaries. rigetti does the task by gate decomposition. A random unitary can be represeted by rotation matrices like this:
+# all we need in the protocal is the random unitaries. rigetti does the task by gate decomposition. A random special unitary can be represeted by rotation matrices like this:
 # $$
-# U = R_Z(\phi) R_Y(\theta)\\
+# U = R_Z(\omega) R_Y(\theta) R_Z(\phi)\\
 # $$
 # Which in terms of the shortest depth of Rigetti native circuits is:
 # $$
-# U = R_Z(\phi) R_X(\frac{\pi}{2}) R_Z(\theta) R_X( -\frac{\pi}{2}) \\
+# U = R_Z(\omega) R_X(\frac{\pi}{2}) R_Z(\theta) R_X( -\frac{\pi}{2}) R_Z(\phi)\\
 # $$
-# If we take a uniform distribution over U, it induces the following conditional distribution over $\theta$ and $\phi$:
+# If we take a uniform distribution over U, it induces the following conditional distribution over $\phi, \omega$ and $\theta$:
 # $$
 # P\{ \theta \} = \frac{\sin \theta}{2\pi}\\
-# P\{ \phi | \theta \} = \frac{1}{2\pi}
+# P\{ \phi | \theta \} = \frac{1}{2\pi} \\
+# P\{ \omega | \theta,\phi \} = \frac{1}{2\pi}
 # $$
 
 # In this project we benchmark with those conditional probabilities
@@ -53,8 +54,8 @@ if __name__ == "__main__":
 
 #     First step choose m and the K_m sequences of 
     m = 5
-    k_m = 10 #n. of diff sequences
-    n_m = 10  #n. of samples from a certain sequence
+    k_m = 1 #n. of diff sequences
+    n_m = 1  #n. of samples from a certain sequence
 
 
 # In[ ]:
@@ -66,18 +67,17 @@ if __name__ == "__main__":
 # In[5]:
 
 
-def native_rigetti_packs_generator(num_qubit,num_layer):
+def native_rigetti_packs_generator(target_qubit:int, num_layer):
     list_gates = []
     angles = np.linspace(0, np.pi, 100)
     
-    for index in range(num_layer):
-        phi = np.random.uniform(0, 2*np.pi)
+    for index in range(0,num_layer):
+        omega, phi = np.random.uniform(0, 2*np.pi, size = 2)
         theta = np.random.choice(angles, p = np.sin(angles) / np.sum( np.sin(angles) ))
 
-        list_gates.extend( [RX(-np.pi/2, qubit = 0),
-                            RZ(theta, qubit = 0),
-                            RX(np.pi/2, qubit = 0),
-                            RZ(phi, qubit = 0)] )
+        list_gates.extend( [RZ(phi, qubit = target_qubit),
+                            RY(theta, qubit = target_qubit),
+                            RZ(omega, qubit = target_qubit)] )
     return list_gates
 
 
@@ -93,10 +93,10 @@ def native_rigetti_packs_generator(num_qubit,num_layer):
 
 
 
-# In[6]:
+# In[11]:
 
 
-def machine_response_rb_native_gate_conditional(qmachine, num_qubits, m, k_m, n_m):
+def machine_response_rb_native_gate_conditional_single_qubit(qmachine, target_qubit, m, k_m, n_m):
     """
     It samples and record the accept or reject of the machine with native gates chosen with conditions for rigetti.
     ::return response_matrix including accepts and rejects in columns
@@ -104,26 +104,29 @@ def machine_response_rb_native_gate_conditional(qmachine, num_qubits, m, k_m, n_
     response_matrix = np.zeros((k_m,n_m))
     
     for i_sequ in range(k_m):
-        gate_list = native_rigetti_packs_generator(num_qubits, m)
+        gate_list = native_rigetti_packs_generator(target_qubit, m)
         prog = Program() #All qubits begin with |0> state
         
         for gate in gate_list:
             prog += gate
         
         #Come back to our initial state
-        for gate in reversed(gate_list):
-#             prog += copy.deepcopy(gate).dagger() #dagger has replacing operations
-            gate_daggered = copy.deepcopy(gate)
-            gate_daggered.params[0] *= -1 #make daggered rotation 
-            prog += gate_daggered
+#         for gate in reversed(gate_list):
+# #             prog += copy.deepcopy(gate).dagger() #dagger has replacing operations
+#             gate_daggered = copy.deepcopy(gate)
+#             gate_daggered.params[0] *= -1 #make daggered rotation 
+#             prog += gate_daggered
             
+        u_inverse = DefGate('U_inverse', np.linalg.inv(program_unitary(prog, n_qubits=1)))
+        prog += qmachine.compiler.quil_to_native_quil(Program(u_inverse))
+        
         #Do not let the quilc to alter the gates by optimization
         prog = Program('PRAGMA PRESERVE_BLOCK') + prog
         prog += Program('PRAGMA END_PRESERVE_BLOCK')
         
         #Measurments
-        ro = prog.declare('ro', 'BIT', num_qubits)
-        for q in range(num_qubits):
+        ro = prog.declare('ro', 'BIT', 1)
+        for q in range(1):
             prog += MEASURE(q, ro[q])
         prog = prog.wrap_in_numshots_loop(n_m)
 
@@ -136,25 +139,20 @@ def machine_response_rb_native_gate_conditional(qmachine, num_qubits, m, k_m, n_
     return response_matrix
 
 
+# In[10]:
+
+
+if __name__ == "__main__":
+    get_ipython().system('jupyter nbconvert RB_with_Rigetti_native_gates_conditional_probability.ipynb --to python')
+
+
 # In[8]:
 
 
 if __name__ == "__main__":
-    get_ipython().system('jupyter nbconvert --to python RB_with_Rigetti_native_gates_conditional_probability.ipynb')
-
-
-# In[12]:
-
-
-if __name__ == "__main__":
 #     qc = get_qc( str(num_qubits) + 'q-qvm')  # You can make any 'nq-qvm'
-#     qc = get_qc("9q-square-noisy-qvm")
-    qmachine = get_qc( str(3) + 'q-qvm')
-    epsilon = 1
-    p_xi = epsilon/4
-    qmachine.qam.gate_noise=(p_xi,p_xi,p_xi)
-    response = machine_response_rb_native_gate_conditional(qmachine,num_qubits, m, k_m, n_m)
-    
+    qc = get_qc("9q-square-noisy-qvm")
+    response = machine_response_rb_native_gate_conditional_single_qubit(qc, 0, m, k_m, n_m)
 
 
 # In[ ]:
